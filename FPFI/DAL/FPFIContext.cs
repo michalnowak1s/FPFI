3fd0c6c6170c9a32099143f3cf29ddc9bf04b67d
unknown <mateuszk-98@wp.pl>
2023-05-07 09:25:32 +0200

    Dodanie DBContext

---
+++ FPFI/DAL/FPFIContext.cs
@@ -0,0 +1,29 @@
+﻿using FPFI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace FPFI.DAL
+{
+    public class FPFIContext : DbContext
+    {
+        public FPFIContext() : base("FPFIContext")
+        {
+        }
+
+        public DbSet<Account> Account { get; set; }
+        public DbSet<Ingredients> Ingredients { get; set; }
+        public DbSet<MealPlans> MealPlans { get; set; }
+        public DbSet<Meals> Meals { get; set; }
+        public DbSet<MealsIngredients> MealsIngredients { get; set; }
+
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
