8c2a1fbc94ae12559854d6bf238acc90fd9b4900
unknown <mateuszk-98@wp.pl>
2023-05-07 10:32:07 +0200

    Poprawki Michała

--- FPFI/Models/Ingredients.cs
+++ FPFI/Models/Ingredients.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,8 @@ namespace FPFI.Models
 {
     public class Ingredients
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int IngredientID { get; set; }
         public string Name { get; set; }
         public string Type { get; set; }
