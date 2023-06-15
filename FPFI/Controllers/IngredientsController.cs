a57f3b4cf4c26f31ae84b337ffb22f68966eb342
makka999 <mateuszk-98@wp.pl>
2023-06-15 19:41:20 +0200

    Ingredient działa CRUD

---
+++ FPFI/Controllers/IngredientsController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FPFI.DAL;
+using FPFI.Models;
+
+namespace FPFI.Controllers
+{
+    public class IngredientsController : Controller
+    {
+        private FPFIContext db = new FPFIContext();
+
+        // GET: Ingredients
+        public ActionResult Index()
+        {
+            var ingredients = db.Ingredients.Include(i => i.Unit);
+            return View(ingredients.ToList());
+        }
+
+        // GET: Ingredients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Create
+        public ActionResult Create()
+        {
+            ViewBag.UnitID = new SelectList(db.Units, "UnitID", "Name");
+            return View();
+        }
+
+        // POST: Ingredients/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IngredientID,Name,Type,Nutriscore,Endurance,Allergen,UnitID")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Ingredients.Add(ingredient);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UnitID = new SelectList(db.Units, "UnitID", "Name", ingredient.UnitID);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UnitID = new SelectList(db.Units, "UnitID", "Name", ingredient.UnitID);
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IngredientID,Name,Type,Nutriscore,Endurance,Allergen,UnitID")] Ingredient ingredient)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ingredient).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UnitID = new SelectList(db.Units, "UnitID", "Name", ingredient.UnitID);
+            return View(ingredient);
+        }
+
+        // GET: Ingredients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ingredient ingredient = db.Ingredients.Find(id);
+            if (ingredient == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ingredient);
+        }
+
+        // POST: Ingredients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ingredient ingredient = db.Ingredients.Find(id);
+            db.Ingredients.Remove(ingredient);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
