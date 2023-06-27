6b6aad638002d8178f34ccdee565dd1dce519872
Hubert <hubertmatlak7@gmail.com>
2023-06-27 22:01:10 +0200

    uporzadkowanie inicjalizacji bazy danych podstawowymi danymi

--- FPFI/Global.asax.cs
+++ FPFI/Global.asax.cs
@@ -1,4 +1,5 @@
 using FPFI.DAL;
+using FPFI.Migrations;
 using FPFI.Models;
 using System;
 using System.Collections.Generic;
@@ -18,94 +19,79 @@ namespace FPFI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
+            
+            
             using (var context = new FPFIContext())
             {
-                var meals = new List<Meal>
-            {
-            new Meal{Name="Kluski", Nationality="silesia", Difficulty="3", Nutriscore="A", Type="klyuskowate", TimeConsumption="2h", AccountID=3},
-            new Meal{Name="Paruwy", Nationality="golen", Difficulty="1", Nutriscore="F", Type="szypkie", TimeConsumption="10m", AccountID=3},
-            new Meal{Name="ParuwyParowe", Nationality="germany", Difficulty="2", Nutriscore="E", Type="parowe", TimeConsumption="20m", AccountID=3},
-            new Meal{Name="ModroKapusta", Nationality="silesia", Difficulty="5", Nutriscore="C", Type="slunskie", TimeConsumption="3h", AccountID = 3},
-            new Meal{Name="TortilieKurczak", Nationality="mexico", Difficulty="2", Nutriscore="B", Type="tortilie", TimeConsumption="15m", AccountID = 3},
-            };
-                meals.ForEach(s => context.Meal.Add(s));
+
+                //Najpierw podstawowe
+
+               /* var accountInitial = new List<Account>
+                {
+                new Account{Login="Carson",Password="Alexander"},
+                new Account{Login="Meredith",Password="Alonso"},
+                new Account{Login="Arturo",Password="Anand"},
+                new Account{Login="Gytis",Password="Barzdukas"},
+                new Account{Login="Yan",Password="Li"},
+                new Account{Login="Peggy",Password="Justice"},
+                new Account{Login="Laura",Password="Norman"},
+                new Account{Login="Nino",Password="Olivetto"}
+                };
+                accountInitial.ForEach(s => context.Accounts.Add(s));
                 context.SaveChanges();
 
+                var mealsInitial = new List<Meal>
+                {
 
+                    new Meal { Name = "Kluski", Nationality = "silesia", Difficulty = "3", Nutriscore = "A", Type = "klyuskowate", TimeConsumption = "2h", AccountID = 3 },
+                    new Meal { Name = "Paruwy", Nationality = "golen", Difficulty = "1", Nutriscore = "F", Type = "szypkie", TimeConsumption = "10m", AccountID = 3 },
+                    new Meal { Name = "ParuwyParowe", Nationality = "germany", Difficulty = "2", Nutriscore = "E", Type = "parowe", TimeConsumption = "20m", AccountID = 3 },
+                    new Meal { Name = "ModroKapusta", Nationality = "silesia", Difficulty = "5", Nutriscore = "C", Type = "slunskie", TimeConsumption = "3h", AccountID = 3 },
+                    new Meal { Name = "TortilieKurczak", Nationality = "mexico", Difficulty = "2", Nutriscore = "B", Type = "tortilie", TimeConsumption = "15m", AccountID = 3 },
+                };
+                mealsInitial.ForEach(s => context.Meal.Add(s));
+                context.SaveChanges();
 
-
-                var units = new List<Unit>
+                var unitsInitial = new List<Unit>
                 {
                     new Unit {Name="Gramy"},
                     new Unit {Name="Szczypta"},
 
                 };
-                units.ForEach(u => context.Units.Add(u));
+                unitsInitial.ForEach(u => context.Units.Add(u));
                 context.SaveChanges();
+
+                var ingredientsInitial = new List<Ingredient>
+                {
+                new Ingredient{Name="PenautButter", Allergen="nuts", Endurance="5", Nutriscore="A", Type="vegetables", UnitID=1},
+                new Ingredient{Name="Broccoli", Endurance="15", Nutriscore="C", Type="vegetables", UnitID=1},
+                new Ingredient{Name="OliveOil", Endurance="5", Nutriscore="E", Type="oils", UnitID=1},
+                new Ingredient{Name="Apple", Endurance="5", Nutriscore="B", Type="fruits", UnitID=1},
+
+                };
+                ingredientsInitial.ForEach(s => context.Ingredients.Add(s));
+                context.SaveChanges();
+
+                
+
+                //Przy nastepnym odpaleniu tabela z relacjami 
+
+                var mealsIngredientsInitial = new List<MealIngredient>
+                {
+                new MealIngredient{MealID=1, IngredientID=3, Quantity= 15},
+                new MealIngredient{MealID=1, IngredientID=4, Quantity= 10},
+                new MealIngredient{MealID=1, IngredientID=6, Quantity= 5},
+                new MealIngredient{MealID=2, IngredientID=5, Quantity= 3},
+                new MealIngredient{MealID=3, IngredientID=6, Quantity= 15},
+
+                };
+                mealsIngredientsInitial.ForEach(s => context.MealIngredients.Add(s));
+                context.SaveChanges();*/
+
             }
-
-
-            //    var students = new List<Account>
-            //{
-            //new Account{Login="Carson",Password="Alexander"},
-            //new Account{Login="Meredith",Password="Alonso"},
-            //new Account{Login="Arturo",Password="Anand"},
-            //new Account{Login="Gytis",Password="Barzdukas"},
-            //new Account{Login="Yan",Password="Li"},
-            //new Account{Login="Peggy",Password="Justice"},
-            //new Account{Login="Laura",Password="Norman"},
-            //new Account{Login="Nino",Password="Olivetto"}
-            //};
-            //    students.ForEach(s => context.Accounts.Add(s));
-            //    context.SaveChanges();
-
-            //    var units = new List<Unit>
-            //{
-            //    new Unit {Name="Gramy"}
-            //};
-            //    units.ForEach(s => context.Units.Add(s));
-            //    context.SaveChanges();
-
-            //    var ingredients = new List<Ingredient>
-            //{
-            //new Ingredient{Name="PenautButter", Allergen="nuts", Endurance="5", Nutriscore="A", Type="vegetables", UnitID=1},
-            //new Ingredient{Name="Broccoli", Endurance="15", Nutriscore="C", Type="vegetables", UnitID=1},
-            //new Ingredient{Name="OliveOil", Endurance="5", Nutriscore="E", Type="oils", UnitID=1},
-            //new Ingredient{Name="Apple", Endurance="5", Nutriscore="B", Type="fruits", UnitID=1},
-
-            //};
-            //    ingredients.ForEach(s => context.Ingredients.Add(s));
-            //    context.SaveChanges();
-
-            /* var meals = new List<Meal>
-         {
-         new Meal{Name="Kluski", Nationality="silesia", Difficulty="3", Nutriscore="A", Type="klyuskowate", TimeConsumption="2h", AccountID=1},
-         new Meal{Name="Paruwy", Nationality="golen", Difficulty="1", Nutriscore="F", Type="szypkie", TimeConsumption="10m", AccountID=1},
-         new Meal{Name="ParuwyParowe", Nationality="germany", Difficulty="2", Nutriscore="E", Type="parowe", TimeConsumption="20m", AccountID=1},
-         new Meal{Name="ModroKapusta", Nationality="silesia", Difficulty="5", Nutriscore="C", Type="slunskie", TimeConsumption="3h", AccountID = 1},
-         new Meal{Name="TortilieKurczak", Nationality="mexico", Difficulty="2", Nutriscore="B", Type="tortilie", TimeConsumption="15m", AccountID = 1},
-         };
-             meals.ForEach(s => context.Meal.Add(s));
-             context.SaveChanges();*/
-
-
-            //    var mealsingredients = new List<MealIngredient>
-            //{
-            //new MealIngredient{MealID=1, IngredientID=3, Quantity= 15},
-            //new MealIngredient{MealID=1, IngredientID=4, Quantity= 10},
-            //new MealIngredient{MealID=1, IngredientID=6, Quantity= 5},
-            //new MealIngredient{MealID=2, IngredientID=5, Quantity= 3},
-            //new MealIngredient{MealID=3, IngredientID=6, Quantity= 15},
-
-            //};
-            //    meals.ForEach(s => context.Meals.Add(s));
-            //    context.SaveChanges();
-            //}
-
         }
     }
-    }
+}
 
     
 
